e557021b081f1c5e110e7a586517bc1825f5513c
Dvk98 <dennisvonknethen@web.de>
2018-11-13 22:19:42 +0100

    first

---
+++ QuizProjec/Map.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Map
+{
+public:
+	Map();
+	sf::Texture getTexture();
+
+private:
+
+
+private:
+
+};
---
+++ QuizProjec/World.cpp
@@ -0,0 +1,16 @@
+#include "World.hpp"
+
+World::World()
+{
+
+}
+
+void World::draw(sf::RenderTarget& target, sf::RenderStates states)
+{
+
+}
+
+void World::update(sf::Time elapsedtime)
+{
+
+}
